859951a6fef67f35983476c60487000087957056
narynbaev.iskander <inarynbaev@beeline.kz>
2023-07-24 22:43:09 +0600

    изучил структуру : 1 этап

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+
+    let person = Person {
+        name: String::from("Alex"),
+        age: 32,
+        sex: true,
+        salary: 14342.444,
+    };
+    println!("{:#?}",person);
+
+    //------------------
+    let log = Info (String::from("Success"),1,true);
+    println!("{:#?}",log);
+
 }
+
+
+//simple struct
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+    sex: bool,
+    salary: f32
+}
+
+// cortage struct
+#[derive(Debug)]
+struct Info (String, u64, bool);
