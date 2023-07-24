be1d11a98fc81150bd5d11cb8c9d1c1d213d5777
narynbaev.iskander <inarynbaev@beeline.kz>
2023-07-24 23:02:53 +0600

    изучил структуру : 2 этап добавление методов

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 fn main() {
 
-    let person = Person {
-        name: String::from("Alex"),
-        age: 32,
-        sex: true,
-        salary: 14342.444,
+    // let person = Person {
+    //     name: String::from("Alex"),
+    //     age: 32,
+    //     sex: true,
+    //     salary: 14342.444,
+    // };
+    // println!("{:#?}",person);
+    //
+    // //------------------
+    // let log = Info (String::from("Success"),1,true);
+    // println!("{:#?}",log);
+
+    let triangle = Triangle {
+        c1: 3.0,
+        c2: 4.0,
     };
-    println!("{:#?}",person);
-
-    //------------------
-    let log = Info (String::from("Success"),1,true);
-    println!("{:#?}",log);
-
+    println!("hypotenuza is {}", triangle.find_hyp())
 }
 
 
@@ -27,3 +32,16 @@ struct Person {
 // cortage struct
 #[derive(Debug)]
 struct Info (String, u64, bool);
+
+struct Triangle {
+    c1: f32,
+    c2: f32
+}
+
+// implement method in struct
+impl Triangle {
+    fn find_hyp(self) -> f32{
+        (self.c1 * self.c1 + self.c2 * self.c2).sqrt()
+    }
+}
+
